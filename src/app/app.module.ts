a61983290402eafeb3a3f0d3e3f09aa2440e17a4
Courtney Jooste <courtney@codelab.io>
2017-08-16 15:58:37 +0200

    Separated app into home and navbar components, added router and more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 import {MdToolbarModule} from '@angular/material';
 import {MdIconModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+// import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import 'hammerjs';
+
+
+
+/*============================================================================
+ Route Imports
+ ============================================================================*/
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './partials/navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, MdButtonModule, MdCheckboxModule, MdToolbarModule, MdIconModule
+    BrowserModule, BrowserAnimationsModule, FormsModule, HttpModule, AppRoutingModule, MdButtonModule, MdCheckboxModule, MdToolbarModule, MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
