fdde6cda1c7a2ea3b7d6808edf122b152c384fa6
Carl Eiserman <carleiserman@gmail.com>
2017-08-17 13:07:06 +0200

    feature: fixed for loops, created proper constructor for home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    images: string[];
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.images = ["../assets/alex-logo.png", "../assets/alex-logo.png", "../assets/alex-logo.png"];
+    }
 
-  images = ["../assets/alex-logo.png", "../assets/alex-logo.png", "../assets/alex-logo.png"];
+    ngOnInit() {
+    }
+
+    giefImages(image: string): any {
+        return {'background': 'url("' + image + '") no-repeat scroll center center / 80px 80px'};
+    }
+
+
 }
