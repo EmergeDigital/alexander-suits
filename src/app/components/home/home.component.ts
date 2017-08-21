a791de9814dc1311113ffa36fe49642381202c37
Courtney Jooste <courtney@codelab.io>
2017-08-21 14:56:32 +0200

    Added testimonials section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,9 +8,32 @@ import {Component, OnInit} from '@angular/core';
 export class HomeComponent implements OnInit {
 
     images: string[];
+    testimonials: object[];
 
     constructor() {
         this.images = ["../assets/accessories.jpg", "../assets/business.jpg", "../assets/suit-up.jpg"];
+        this.testimonials = [
+          {
+            name: "Jim Johnson",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vestibulum magna at mauris dignissim consectetur."
+          },
+          {
+            name: "James Williams",
+            text: "I would buy a suit every day of the week"
+          },
+          {
+            name: "José Luis",
+            text: "Que? No hablo Anglais"
+          },
+          {
+            name: "Quigon Jin",
+            text: "Do you know which sector we are in?"
+          },
+          {
+            name: "Brodo Faggins",
+            text: "The best wares in all of Hobbiton"
+          }
+        ];
     }
 
     ngOnInit() {
