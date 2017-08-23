7e57f30c3d75e7fca134ed6e2f141a0908b8d829
Courtney Jooste <courtney@codelab.io>
2017-08-23 14:24:58 +0200

    Added snackbar notifications to about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MdSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(public snackBar: MdSnackBar) { }
 
   ngOnInit() {
   }
-
+  
+  doesNothing() {
+    this.snackBar.open("This does nothing at the moment!", "", {  duration: 2000   });
+  }
 }
