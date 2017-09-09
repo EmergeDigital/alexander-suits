a8e528229d424e1ab1a52a84547a33fa96ac0549
Courtney Jooste <courtney@codelab.io>
2017-09-09 13:29:58 +0200

    Renamed checkout component

--- src/app/components/account/checkout/checkout.component.ts
+++ src/app/components/account/checkout/checkout.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-checkout',
+  selector: 'app-checkout-main',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutMainComponent implements OnInit {
 
   constructor() { }
 
