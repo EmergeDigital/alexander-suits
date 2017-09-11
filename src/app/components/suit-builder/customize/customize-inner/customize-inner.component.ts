92751937834f5d7a860152d4d22255b12d9cae7e
Courtney Jooste <courtney@codelab.io>
2017-09-11 10:45:34 +0200

    Created demo stepper for suit builder

--- src/app/components/suit-builder/customize/customize-inner/customize-inner.component.ts
+++ src/app/components/suit-builder/customize/customize-inner/customize-inner.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Http, Headers, HttpModule} from '@angular/http';
-import {RequestOptions, Request, RequestMethod} from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 // import 'rxjs/add/operator/map';
@@ -12,60 +10,50 @@ import 'rxjs/add/operator/toPromise';
 })
 export class CustomizeInnerComponent implements OnInit {
 
-  constructor(private http: Http) { }
+  s0: any;
+  s1: any;
+  s2: any;
+  s3: any;
+  completed0: boolean;
+  step: string;
+  steps: any[];
 
-  authToken: string;
+  constructor() {
+    this.s0 = {
+      'display': 'block'
+    };
+    this.s1 = {
+      'display': 'none'
+    };
+    this.s2 = {
+      'display': 'none'
+    };
+    this.s3 = {
+      'display': 'none'
+    };
+
+    this.step = "step0";
+    this.completed0 = false;
+    this.steps = [this.s0, this.s1, this.s2, this.s3];
+  }
 
   ngOnInit() {
   }
 
-  startLogin() {
-    this.login().then((success)=>
-      {
-        console.log(success);
-      })
+  changeStep(s){
+    this.s0.display = 'none';
+    this.s1.display = 'none';
+    this.s2.display = 'none';
+    this.s3.display = 'none';
+
+    // let _step = this.steps[s];
+    this.step = "step" + s;
+
+    this.steps[s].display = 'block';
+
   }
 
-  login() {
 
-    let addr = "http://0.0.0.0:8000/v1/account/login";
-    let payload = {
-      "email": "courtney@codelab.io",
-      "password": "****"
-    };
-
-    return new Promise((resolve, reject) => {
-        let req = this.http.post(addr, payload);
-
-        //REQUEST WITH PARAMS, IMPORT HTTPHEADERS SOMEHOW
-        // let req = this.http.post(addr, payload, {
-        //   headers: new HttpHeaders().set('Authorization', 'my-auth-token'),
-        // });
-
-        //REQUEST WITH PARAMS, IMPORT HTTPPARAMS SOMEHOW
-        // let req = this.http.post('/api/items/add', body, {
-        //   params: new HttpParams().set('id', '3'),
-        // })
-        // 0 requests made - .subscribe() not called.
-        req.subscribe(data => console.log(data));
-        // 1 request made.
-        req.subscribe(data => console.log(data));
-        // 2 requests made.
-    });
-  }
-
-  _wrapAndRequest(request, resolve, reject) {
-      if (this.authToken) {
-          request.set('Authorization', this.authToken);
-      }
-      request.end(function (err, result) {
-          if (err) {
-              reject({status: err.status, result: (result) ? result.body : null});
-          } else {
-              resolve(result.body);
-          }
-      });
-  }
 
 
 }
