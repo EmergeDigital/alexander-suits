586d1d61cfda315aa294fac878f267172cd772bb
Courtney Jooste <courtney@codelab.io>
2017-09-26 11:36:49 +0200

    Added first coat building step

--- src/app/components/suit-builder/customize/customize-inner/customize-inner.component.ts
+++ src/app/components/suit-builder/customize/customize-inner/customize-inner.component.ts
@@ -13,6 +13,18 @@ export class CustomizeInnerComponent implements OnInit {
   stepsLength: number;
   steps: any[];
   completed0: boolean;
+  collars: any[] = [
+    {"name": "1", "url": "assets/suit-builder/collar-v1.png"},
+    {"name": "2", "url": "assets/suit-builder/collar-v2.png"},
+    {"name": "3", "url": "assets/suit-builder/collar-v3.png"},
+    {"name": "4", "url": "assets/suit-builder/collar-v4.png"},
+    {"name": "5", "url": "assets/suit-builder/collar-v5.png"},
+    {"name": "6", "url": "assets/suit-builder/collar-v6.png"},
+    {"name": "7", "url": "assets/suit-builder/collar-v7.png"},
+    {"name": "8", "url": "assets/suit-builder/collar-v8.png"},
+    {"name": "9", "url": "assets/suit-builder/collar-v11.png"},
+  ];
+  selectedCollar: any = {"name": "1", "url": "assets/suit-builder/collar-v1.png"};
 
   constructor() {
     this.steps = [];
@@ -26,6 +38,10 @@ export class CustomizeInnerComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  selectCollar(collar) {
+    this.selectedCollar = collar;
   }
 
   changeStep(s){
