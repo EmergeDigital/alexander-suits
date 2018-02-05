62e15f9d1570ba8c6968c4c464fc54a569a30710
Sabeeha Banubhai <sabeeha.banubhai@eoh.com>
2018-02-05 21:39:40 +0200

    WIP - SC: Fabrics - Design Updates

--- src/app/components/suit-builder/fabric/material/material.component.ts
+++ src/app/components/suit-builder/fabric/material/material.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'suit-builder-fabric-material',
   templateUrl: './material.component.html',
-  styleUrls: ['./material.component.scss']
+  styleUrls: ['./material.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MaterialComponent implements OnInit {
 
